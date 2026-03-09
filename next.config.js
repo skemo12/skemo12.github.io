adb7bc8e493a79f24aee0cde7ed6e6853c711e0f
Darius Paunoiu <dariuspaunoiu@gmail.com>
2026-03-09 14:24:24 +0200

    Make for GitHub and CF Pages work

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const basePath = process.env.BASE_PATH || "";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const basePath = isGithubPages ? process.env.BASE_PATH || "" : "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: isGithubPages ? "export" : "standalone",
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
   basePath,
-  assetPrefix: basePath
+  assetPrefix: isGithubPages ? basePath : undefined
 };
 
 module.exports = nextConfig;
