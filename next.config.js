c2c9ab34468188c77de6cd116d06b3a0185850ee
Darius Paunoiu <dariuspaunoiu@gmail.com>
2026-03-09 14:38:14 +0200

    Switch to only GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const basePath = isGithubPages ? process.env.BASE_PATH || "" : "";
+const basePath = process.env.BASE_PATH || "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: isGithubPages ? "export" : "standalone",
+  output: "export",
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true
   },
   basePath,
-  assetPrefix: isGithubPages ? basePath : undefined
+  assetPrefix: basePath ? basePath : undefined
 };
 
 module.exports = nextConfig;
